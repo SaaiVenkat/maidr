b75a468331fd625d9192afbbd33c46132ed4a9ba
SaaiVenkat <greenghost1100@gmail.com>
2024-01-25 22:01:44 -0600

    chore: updated chart to include single type of scatterplots

--- examples/matplotlib/scatterplot/point/scatterplot.py
+++ examples/matplotlib/scatterplot/point/scatterplot.py
@@ -3,26 +3,29 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import json
 
-# Load an example dataset from seaborn
+# Load the penguins dataset
 data = sns.load_dataset('penguins')
 
-# Create a scatter plot with the seaborn dataset
+# Filter the data for only the 'Adelie' species
+data_adelie = data[data['species'] == 'Adelie']
+
+# Create a scatter plot with the seaborn dataset for only the 'Adelie' species
 plt.figure(figsize=(10, 6))
-scatter_plot = sns.scatterplot(data=data, x="bill_length_mm", y="bill_depth_mm", hue="species", style="species", palette="deep")
-plt.title('Penguin bill dimensions by species')
+scatter_plot = sns.scatterplot(data=data_adelie, x="bill_length_mm", y="bill_depth_mm", hue="species", style="species", palette="deep")
+plt.title('Adelie Penguin Bill Dimensions')
 plt.xlabel('Bill Length (mm)')
 plt.ylabel('Bill Depth (mm)')
 
 # Save the plot as an SVG file
 plt.savefig('scatterplot.svg', format='svg')
 
-# Generating the 'maidr' structure
+# Generating the 'maidr' structure for the 'Adelie' species
 maidr = {
-    'type': 'point',
+    'type': ['point'],
     'id': 'scatter',
-    'selector': "TODO: INCLUDE THE POINT SELECTOR HERE",
-    'title': 'Penguin bill dimensions by species',
-    'name': 'Scatterplot',
+    'selector': ["TODO: INCLUDE THE POINT SELECTOR HERE"],
+    'title': 'Adelie Penguin Bill Dimensions',
+    'name': 'Scatterplot - Adelie',
     'axes': {
         'x': {
             'label': 'Bill Length (mm)',
@@ -32,19 +35,14 @@ maidr = {
         },
     },
     'labels': {
-        'title': 'Penguin bill dimensions by species',
+        'title': 'Adelie Penguin Bill Dimensions',
         'x': 'Bill Length (mm)',
         'y': 'Bill Depth (mm)',
     },
-    'data': [
-        [
-            {
-                'x': row['bill_length_mm'],
-                'y': row['bill_depth_mm'],
-            }
-            for _, row in data.iterrows() if not (pd.isnull(row['bill_length_mm']) or pd.isnull(row['bill_depth_mm']))
-        ]
-    ],
+    'data': [{
+         'x': data_adelie['bill_length_mm'].dropna().tolist(),
+         'y': data_adelie['bill_depth_mm'].dropna().tolist()
+     }],
 }
 
 # Writing the raw data to a JSON file
@@ -53,7 +51,7 @@ with open("scatterplot_raw_data.json", 'w') as json_file:
 
 # Writing the dictionary to a JSON file
 with open("scatterplot_maidr.json", 'w') as json_file:
-    json.dump(maidr, json_file, indent=4)   
+    json.dump(maidr, json_file, indent=4)
 
 # Display the plot
 plt.show()
